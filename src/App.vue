89b8d4c6d16fea678f5736be297d20d67725c225
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2021-10-22 14:43:42 -0300

    3 commit

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,40 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+      <Navbar />
+      <v-parallax
+      height="100%"
+    dark
+    src="assets\Crypto Fund Logo.png"
+    contain
+  >
+  <br>
+       <transition mode="out-in" name="fade">
+          <router-view  />
+        </transition>
+        
+      </v-parallax>
+      <Footer/>
+    </v-app>
+  
 </template>
 
 <script>
 export default {
   name: "App",
 
-  data: () => ({
-    //
-  }),
+  components: {
+    Navbar: () => import("./components/App/Navbar"),
+    Footer: () => import("./components/App/Footer.vue")
+  },
 };
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,47 @@
 <template>
-  <hello-world />
+<v-section>
+  <v-carousel cycle :show-arrows="false" hide-delimiters>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      contain
+    ></v-carousel-item>
+  </v-carousel>
+
+</v-section>
+  <br>
+  <v-section >
+    <v-img></v-img>
+
+  </v-section>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
-export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png',
+          },
+          {
+            src: 'https://lineadecodigo.com/wp-content/uploads/2014/04/css.png',
+          },
+          {
+            src: 'https://cuos.com.ar/img/tecnologias/js.png',
+          },
+          {
+            src: 'https://i.blogs.es/545cf8/es6-logo/450_1000.png',
+          },
+          {
+            src: 'https://thumbs.gfycat.com/PinkPiercingBull-size_restricted.gif'
+          },
+          {
+            src: 'https://miro.medium.com/max/512/1*9U1toerFxB8aiFRreLxEUQ.png'
+          }
+        ],
+      }
+    },
+  }
 </script>
