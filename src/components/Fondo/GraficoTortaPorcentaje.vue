7c63ca59a1496948964231f0a99238194a044a00
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2021-11-02 19:26:17 -0300

    13 commit

---
+++ src/components/Fondo/GraficoTortaPorcentaje.vue
@@ -0,0 +1,143 @@
+<template>
+  <div id="app">
+    <v-card class="mx-auto">
+      
+
+      <v-container fluid>
+        <v-row dense>
+          <v-col>
+            <v-card max-width="344" outlined elevation="2" class="pa-5">
+                <h3>Criptomonedas Agrupadas</h3>
+              <v-btn
+                elevation="2"
+                dark
+                raised
+                color="cforange"
+                @click="addExperience"
+                >Añadir Participante</v-btn
+              >
+              <div class="Chart">
+                <DoughnutExample
+                  ref="skills_chart"
+                  :chart-data="chartData"
+                  :options="options"
+                >
+                </DoughnutExample>
+                <v-layout row wrap>
+                  <v-flex>
+                    <div v-for="(val, i) in currentDataSet" :key="i">
+                      <v-row no-gutters>
+                        <v-col cols="12" sm="6" md="8">
+                          <input
+                            type="range"
+                            min="0"
+                            max="100"
+                            placeholder="coin"
+                            :value="currentDataSet[i]"
+                            @input="updateAmount($event.target.value, i)"
+                          />
+                          <h3>{{ currentDataSet[i] }} %</h3>
+                          <input
+                            type="text"
+                            :value="chartData.labels[i]"
+                            @input="updateName($event.target.value, i)"
+                          />
+                          <v-btn color="error" @click="remove(i)"
+                            >Eliminar</v-btn
+                          >
+                        </v-col>
+                        <v-spacer></v-spacer>
+                        <v-col> </v-col>
+                        <v-spacer></v-spacer>
+                        <v-col cols="2"> </v-col>
+                        <v-col cols="2"> </v-col>
+                      </v-row>
+
+                      <v-spacer></v-spacer>
+                    </div>
+                  </v-flex>
+                </v-layout>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+    <v-layout> </v-layout>
+  </div>
+</template>
+
+<script>
+import DoughnutExample from "@/components/DoughnutExample";
+import randomColor from "randomcolor";
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  animation: {
+    animateRotate: false,
+  },
+};
+
+export default {
+  name: "App",
+  components: { DoughnutExample },
+  data() {
+    return {
+      options,
+      chartData: {
+        labels: ["Coin"],
+        datasets: [
+          {
+            backgroundColor: [randomColor()],
+            data: [1],
+          },
+        ],
+      },
+    };
+  },
+  computed: {
+    currentDataSet() {
+      return this.chartData.datasets[0].data;
+    },
+  },
+  methods: {
+    updateChart() {
+      this.$refs.skills_chart.update();
+    },
+    updateAmount(amount, index) {
+      this.chartData.datasets[0].data.splice(index, 1, amount);
+      this.updateChart();
+    },
+    updateName(text, index) {
+      this.chartData.labels.splice(index, 1, text);
+      this.updateChart();
+    },
+    addExperience() {
+      const currentDataset = this.chartData.datasets[0];
+      this.chartData.labels.push(`${currentDataset.data.length + 1} coin`);
+      currentDataset.data.push(0);
+      currentDataset.backgroundColor.push(randomColor());
+      this.updateChart();
+    },
+    remove(index) {
+      const currentDataset = this.chartData.datasets[0];
+      currentDataset.data.splice(index, 1);
+      this.chartData.labels.splice(index, 1);
+      currentDataset.backgroundColor.splice(index, 1);
+      this.updateChart();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+</style>
