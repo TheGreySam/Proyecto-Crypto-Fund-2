ee3d611af9bb83d6bc897c635226cb804d976bf9
Francisco León <22012000fdlq@gmail.com>
2021-11-02 22:15:05 -0300

    Revert "wip"
    This reverts commit eb591ac5a5c303abeb2799eb06a43db9676322ab.

--- src/views/Mercado.vue
+++ src/views/Mercado.vue
@@ -1,50 +1,64 @@
 <template>
-  <v-container>
-    <h1 class="pb-5">Lista de Monedas</h1>
-    <v-card class="mx-auto"  tile>
-      <v-list rounded elevation="2">
-        <v-list-item-group color="cforange">
-          <v-list-item
-            v-for="coin in $store.state.criptoInfo.data"
-            :key="coin.id"
-          >
-            <v-list-item-icon>
-              <v-img :src="coin.image" :alt="coin.name" width="2rem"> </v-img>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="coin.name"> </v-list-item-title>
-            </v-list-item-content>
-            <br />
-            <v-list-item-content>
-              <v-list-item-title class="text-uppercase">
-                {{ coin.symbol }}
-              </v-list-item-title>
-            </v-list-item-content>
-            <v-list-item-content>
-              <v-list-item-title>
-                {{ coin.current_price }} USD
-              </v-list-item-title>
-            </v-list-item-content>
-            <v-list-item-content>
-              <v-list-item-title>
-                Volumen 24H {{ coin.total_volume }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-card>
-  </v-container>
+  <v-card
+    class="mx-auto"
+    
+    tile
+  >
+  
+    <v-list rounded>
+      <v-subheader></v-subheader>
+      <v-list-item-group
+        
+      
+      >
+        <v-list-item
+          v-for="coin in $store.state.criptoInfo.data"
+          :key="coin.id"
+        >
+          <v-list-item-icon>
+            <v-img 
+            :src="coin.image" 
+            :alt="coin.name" 
+            width="2rem">
+            </v-img>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="coin.name">
+                            
+            </v-list-item-title>
+          </v-list-item-content>
+          <br>
+          <v-list-item-content>
+            <v-list-item-title class="text-uppercase">
+              {{coin.symbol}}
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{coin.current_price}} USD
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-content>
+            <v-list-item-title>
+              Volumen 24H {{coin.total_volume}}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
-export default {
-  name: "App",
-
-  data() {
+  export default {
+    name: "App",
+    
+    data(){
     return {
-      coins: [],
-    };
+      coins: []
+    }
   },
-};
+
+
+  }
 </script>
