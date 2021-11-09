8f251b2999289cfd99b1bb08ec8548a00dd86d24
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2021-11-08 21:30:10 -0300

    17 commit
    conflict merge

--- src/store/modules/criptoInfo.js
+++ src/store/modules/criptoInfo.js
@@ -18,18 +18,5 @@ export const criptoInfoModule = {
         context.commit("SET_DATA", data);
       }, 10000);
     },
-<<<<<<< HEAD
-    actions: {
-        subscribe(context) {
-            setInterval(async () => {
-                const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
-                const data = await res.json();
-                context.commit("SET_DATA", data)
-            }, 1000)
-        }
-    }
-}
-=======
   },
 };
->>>>>>> origin/login-ready
