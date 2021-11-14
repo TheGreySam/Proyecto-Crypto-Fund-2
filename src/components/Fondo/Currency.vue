66d203c200124778dd23b896e0e30689706fc20e
Francisco León <22012000fdlq@gmail.com>
2021-11-14 17:33:16 -0300

    wip

--- src/components/Fondo/Currency.vue
+++ src/components/Fondo/Currency.vue
@@ -1,138 +1,95 @@
 <template>
-  <v-app>
+  <v-card>
     <v-container>
-      <v-row>
-        <v-col cols="10" md="12" lg="9" xl="6" class="bg--light mx-auto">
-          <h1 class="text-center mb-4">Currency Converter</h1>
-          <v-row justify="space-between" no-gutters>
-            <v-col cols="12" md="5">
-              <v-row no-gutters>
-                <v-col class="px-1" cols="3">
-                  <v-select @change="convert" v-model="selected[0]" :items="countries" label="I have"></v-select>
-                </v-col>
+      <h1>Calcula el valor de tus Criptomonedas</h1>
+      <v-select
+        @change="convert"
+        v-model="selected[0]"
+        :items="coins"
+        item-text="name"
+        item-value="current_price"
+        label="I have"
+      ></v-select>
+       <v-select
+        @change="convert"
+        v-model="selected[1]"
+        :items="coins"
+        item-text="name"
+        item-value="current_price"
+        label="I have"
+      ></v-select>
+      <v-text-field @input="convert" v-model="inputed"></v-text-field>
 
-                <v-col class="px-1" cols="9">
-                  <v-text-field @input="convert" v-model="inputed" :rules="[rules.onlyNumbers]"></v-text-field>
-                </v-col>
-              </v-row>
-            </v-col>
-
-           
-
-            <v-col cols="12" md="5">
-              <v-row no-gutters>
-                <v-col class="px-1" cols="9">
-                  <v-text-field v-model="result" type="number" readonly></v-text-field>
-                </v-col>
-
-                <v-col class="px-1" cols="3">
-                  <v-select @change="convert" v-model="selected[1]" :items="countries" label="I want to buy"></v-select>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
+      <h1> Tienes {{ this.result }} </h1>
     </v-container>
-  </v-app>
+  </v-card>
 </template>
 
 <script>
-
-const axios = require('axios').default;
-
 export default {
-
-  name: 'App',
+  name: "App",
 
   data() {
     return {
-      valutes: {},
-      selected: ['RUB', 'USD'],
+      coins: [],
+      selected: [],
       inputed: "",
       result: null,
-      countries: ['RUB'],
-      rules: {
-        onlyNumbers: (value) => {
-          const pattern = /^\d+$/.test(value);
-
-          if (!pattern) this.inputed = this.inputed.replace(/\D/g, '');
-
-          return true;
-        }
-      }
-    }
+      
+    };
   },
 
   methods: {
     convert() {
-      // Default valute values / RUB
-      let defaultValute = {
-        Value: 1,
-        Nominal: 1
-      };
-
       // First selected valute details
-      let firstValute = this.valutes[this.selected[0]] ?? defaultValute,
-          firstValuteValue = firstValute.Value * Number(this.inputed),
-          firstValuteNominal = firstValute.Nominal;
+      let firstValue = this.selected[0];
+      console.log(firstValue);
 
+      let secondValue = this.selected[1];
+      console.log(secondValue);
       // Second selected valute details
-      let secondValute = this.valutes[this.selected[1]] ?? defaultValute,
-          secondValuteValue = secondValute.Value,
-          secondValuteNominal = secondValute.Nominal;
-
-      // Result calculating
-      let result = (firstValuteValue / firstValuteNominal) / (secondValuteValue / secondValuteNominal);
-
+      let result = ( firstValue * this.inputed ) / secondValue;
       // Rounding to ten thousandths
-      this.result = result ? Math.floor(result * 10000) / 10000 : null;
-    }
+      this.result = result
+    },
   },
 
-  mounted() {
-    // Request to API URL, getting response
-    axios
-      .get('https://www.cbr-xml-daily.ru/daily_json.js')
-      .then(response => {
-        this.valutes = response.data.Valute;
-        // Adding all charCodes to array
-        for (let code in response.data.Valute) {
-          this.countries.push(code)
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
+  async mounted() {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=12&page=1&sparkline=false"
+    );
+    const data = await res.json();
+    (this.coins = data), console.log(data);
+    (this.coinsdos = data), console.log(data);
   },
 };
-
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+// @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
 
-@import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
+// #app {
+// 	background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+// 	background-size: 400% 400%;
+// 	animation: gradient 15s ease infinite;
 
-#app {
-	background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-	background-size: 400% 400%;
-	animation: gradient 15s ease infinite;
+//   @keyframes gradient {
+//     0% {
+//       background-position: 0% 50%;
+//     }
+//     50% {
+//       background-position: 100% 50%;
+//     }
+//     100% {
+//       background-position: 0% 50%;
+//     }
+//   }
+// }
 
-  @keyframes gradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-}
-
-h1, h2, h3 {
-  font-family: 'Indie Flower';
+h1,
+h2,
+h3 {
+  font-family: "Indie Flower";
 }
 
 .bg--light {
@@ -142,5 +99,4 @@ h1, h2, h3 {
 .exchange {
   max-width: 50px;
 }
-
 </style>
--- src/views/Fondo.vue
+++ src/views/Fondo.vue
@@ -1,49 +1,48 @@
 <template>
-  <div id="app">
-    <SectionHero msg="Fondos" />
+  <div id="app"  class="cfgray">
+    <SectionHero msg="Fondos" class="pb-5"/>
     <v-container>
       <v-card >
         <v-app-bar
-       class="mx-auto"
-        color="cfdarkblue"
-        dark
-    
-        prominent
-        src="../assets/bitcoin-img.jpeg"
-      fade-img-on-scroll
-        elevate-on-scroll
-        scroll-target="#scrolling-techniques-7"
+          class="mx-auto"
+          color="cfdarkblue"
+          dark
+          prominent
+          src="../assets/bitcoin-img.jpeg"
+          fade-img-on-scroll
+          elevate-on-scroll
+          scroll-target="#scrolling-techniques-7"
         >
-        <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
-        ></v-img>
-      </template>
-        <v-app-bar-nav-icon>
-            <img src="cryptoFundLogo3d.png" width="55" alt="">
-        </v-app-bar-nav-icon>
-        <v-toolbar-title>Billetera 1</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-avatar size="100" class="p-2" tile>
-              <v-img src="wallet.png"></v-img>
-            </v-avatar>
-      </v-app-bar>
+          <template v-slot:img="{ props }">
+            <v-img
+              v-bind="props"
+              gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+            ></v-img>
+          </template>
+          <v-app-bar-nav-icon>
+            <img src="cryptoFundLogo3d.png" width="55" alt="" />
+          </v-app-bar-nav-icon>
+          <v-toolbar-title>Billetera 1</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-avatar size="100" class="p-2" tile>
+            <v-img src="wallet.png"></v-img>
+          </v-avatar>
+        </v-app-bar>
       </v-card>
-      
-      <v-card class="mx-auto">
+
+      <v-card class="mx-auto pb-5 mb-5 cforange">
         <div>
           <v-row>
-            <v-col sm="6" md="5" cols="12">
-              <UserInfo />
-              <Porcentaje />
-              <Currency />
+            <v-col md="6" cols="12">
+              <div class="px-5 py-5"><UserInfo /></div>
+              <div class="px-5 py-5"><Porcentaje /></div>
+              <div class="px-5 py-5"><Currency /></div>
             </v-col>
 
-            <v-col>
+            <v-col md="6" cols="12">
               <GraficoTortaCripto />
             </v-col>
           </v-row>
@@ -52,7 +51,6 @@
     </v-container>
   </div>
 </template>
-
 
 <script>
 import GraficoTortaCripto from "@/components/Fondo/GraficoTortaCripto";
@@ -70,10 +68,9 @@ export default {
     SectionHero,
     UserInfo,
     Porcentaje,
-    Currency
+    Currency,
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
