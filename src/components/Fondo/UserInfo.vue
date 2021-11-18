a5c68e9753e9f18daef8692efc75ab01a1918c80
eolezjua <eolezjua@talana.com>
2021-11-18 16:11:03 -0300

    fondos, imagenes, añadir monedas

--- src/components/Fondo/UserInfo.vue
+++ src/components/Fondo/UserInfo.vue
@@ -1,22 +1,57 @@
 <template>
-  
-          <v-card >
-            <v-card-title class="pa-3 pb-1">
-               <p v-if="this.$store.state.currentUser">Bienvenido {{this.$store.state.currentUser.fullName}}</p>
-          
-            </v-card-title>
-            <v-card-title class="pa-3 pb-1">
+  <v-card
+    class="mx-auto"
+    max-width="550"
+    tile
+  >
+    <v-img
+      height="100%"
+      src="../../assets/bit2.jpeg"
+    >
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="4"
+        >
+          <v-avatar
+            class="profile mt-10 ml-4"
+            size="164"
+            tile
+          >
+            <v-img src="../../assets/avatar33.png"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="8"
+        >
+        <v-card-title class="pa-3 pb-1"
+             v-if="this.$store.state.currentUser.rol == 'admin'" style="background-color: #FA7921">
            
-              <p v-if="this.$store.state.currentUser">Tu Correo es: {{this.$store.state.currentUser.email}}</p>
-            </v-card-title>
-             <v-card-title class="pa-3 pb-1"
-             v-if="this.$store.state.currentUser.rol == 'admin'" style="background-color: blue">
-           
-              <p v-if="this.$store.state.currentUser.rol == 'admin'" style="color: white">Tienes un plan Premium</p>
-              <p v-if="this.$store.state.currentUser.rol == 'user'">Tienes un plan Básico</p>
+              <span v-if="this.$store.state.currentUser.rol == 'admin'" style="color: white">Tienes un plan: Premium</span>
+              <span v-if="this.$store.state.currentUser.rol == 'user'">Tienes un plan: Básico</span>
              
             </v-card-title>
-            
-          </v-card>
-      
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="text-h6" v-if="this.$store.state.currentUser">
+                {{this.$store.state.currentUser.fullName}}
+              </v-list-item-title>
+              <v-list-item-subtitle>{{this.$store.state.currentUser.email}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
 </template>
