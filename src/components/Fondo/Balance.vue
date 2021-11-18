7b9c5d7a2570fc4d972a63ad8ff82ac2051a1960
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2021-11-18 13:52:38 -0300

    Update Balance.vue

--- src/components/Fondo/Balance.vue
+++ src/components/Fondo/Balance.vue
@@ -3,7 +3,7 @@
     <v-card class="mx-1 mb-1 mt-5 p-2">
       <v-container>
           <v-card-title class="pa-3 pb-1">
-               <p v-if="this.$store.state.data">Balance total: {{this.$store.state.data}}</p>
+               <p >Balance total: {{this.totalCoin()}} USD</p>
           
             </v-card-title>
         <v-simple-table class="mb-5">
@@ -22,16 +22,18 @@
               >
                 <td class="text-subtitle-1">{{ data.name }}</td>
                 <td class="text-uppercase text-subtitle-1 font-italic">
-                  {{ data.price }}
+                  {{ data.price }} USD
                 </td>
                 <td class="text-uppercase text-subtitle-1 font-italic">
                   {{ data.valueCoin }}
                 </td>
                 <td class="text-uppercase text-subtitle-1 font-italic">
-                  {{ data.price * data.valueCoin }}
+                  {{ data.price * data.valueCoin }} USD
                 </td>
+               
               </tr>
             </tbody>
+            <v-btn @click="totalCoin">Total</v-btn>
           </template>
         </v-simple-table>
       </v-container>
@@ -44,8 +46,36 @@ export default {
     data() {
     return {
       titles: ["Nombre", "Precio", "Cantidad", "Total"],
-      
+      data: []
     };
   },
+  methods: {
+    totalPrecio() {
+      const precio = this.data.valueCoin * this.data.price
+      console.log(precio)
+
+    },
+    totalCoin() {
+        const total = this.$store.state.data.reduce((sum, value) => (sum +  (value.valueCoin * value.price)), 0);
+        console.log(total)
+         return total;
+         
+         
+        
+    
+    //this.$store.state.data.forEach((coin) => {
+      //let value = coin.valueCoin
+      //let price = coin.price
+      //let total = value * price
+      //let totalTotal = null
+      //totalTotal = total + totalTotal
+      //console.log(totalTotal)
+      //this.totalCoin.push(total)
+    //})
+    
+    //console.log(this.totalCoin)
+  }
+  }
+  
 }
 </script>
